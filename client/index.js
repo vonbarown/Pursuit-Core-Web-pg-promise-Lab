7baccaaad4204b80f81ba132b5872a9f3c6da510
vonbarown <vonielbrown@pursuit.org>
2019-11-02 18:40:03 -0400

    front end

--- client/index.js
+++ client/index.js
@@ -1,13 +1,18 @@
+const port = 3000;
+
 document.addEventListener('DOMContentLoaded', () => {
     loadUsers();
     const form = document.querySelector('#addUserForm');
     form.addEventListener('submit', addUserFormSubmitted);
+
+    loadPosts();
+    const postForm = document.querySelector('#addPosts');
+    postForm.addEventListener('submit', addPostsFormSubmitted);
 });
 
 const loadUsers = async () => {
     const usersList = document.querySelector('#usersList');
     usersList.textContent = "";
-    let port = 3000;
     const {
         data
     } = await axios.get(`http://localhost:${port}/users`);
@@ -31,4 +36,32 @@ const addUserFormSubmitted = async (event) => {
         age
     });
     loadUsers();
+}
+
+const loadPosts = async () => {
+    const postsList = document.querySelector('#postsList');
+    usersList.textContent = "";
+    const {
+        data
+    } = await axios.get(`http://localhost:${port}/posts`);
+    console.log(data);
+
+    data.payload.forEach((user) => {
+        let listItem = document.createElement("li");
+        listItem.innerText = `User: ${user.poster_id}  Posted: ${user.body}`;
+        postsList.appendChild(listItem);
+    });
+}
+
+const addPostsFormSubmitted = async (event) => {
+    event.preventDefault();
+    const poster_id = document.querySelector('#userIdInput').value;
+    const body = document.querySelector('#bodyInput').value;
+    console.log('input', poster_id, body);
+
+    let response = await axios.post(`http://localhost:${port}/posts/register`, {
+        poster_id,
+        body,
+    });
+    loadPosts();
 }
